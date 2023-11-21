8ffb4e6e310d0979fdc59d2c4027101e18a194e1
Ana-Kaleb <Ana-Kaleb@example.com>
2023-11-21 09:41:21 -0300

    Final Front viagem

---
+++ src/app/service/viagem.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ViagemService {
+  private API = '//localhost:8080/viagens';
+
+  constructor(private http: HttpClient) {}
+
+  listar(): Observable<any> {
+    return this.http.get(this.API);
+  }
+  listarById(): Observable<any> {
+    return this.http.get(this.API+"/1")
+  }
+}
